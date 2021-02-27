461e2192badd42e5952cc91cb37a1f74638cb518
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2021-02-27 18:18:34 +0200

    [feature] - Fetch and display orders

--- src/pages/orders/Orders.js
+++ src/pages/orders/Orders.js
@@ -2,10 +2,25 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Table from "react-bootstrap/Table";
 import {Link, useRouteMatch} from "react-router-dom";
+import {useQuery, gql} from "@apollo/client";
+
+const ORDERS = gql`
+  query Orders {
+    orders {
+      id
+      code
+    }
+  }
+`;
 
 const Orders = () => {
   let match = useRouteMatch();
+
+  const {loading, error, data} = useQuery(ORDERS, {
+    fetchPolicy: 'cache-and-network',
+  });
 
   return (
     <Container>
@@ -14,6 +29,30 @@ const Orders = () => {
         <Col>Orders List</Col>
         <Col><Link to={`${match.path}/new`}>New Order</Link></Col>
       </Row>
+      {loading && (
+        <div>Loading...</div>
+      )}
+      {error && (
+        <div>Error loading data!</div>
+      )}
+      {!loading && !error && (
+        <Table striped bordered hover>
+          <thead>
+          <tr>
+            <th>#</th><th>Code</th>
+          </tr>
+          </thead>
+          <tbody>
+          {data.orders.map((order, index) => {
+            return (
+              <tr>
+                <td>{order.id}</td><td>{order.code}</td>
+              </tr>
+            )
+          })}
+          </tbody>
+        </Table>
+      )}
     </Container>
   );
 };
